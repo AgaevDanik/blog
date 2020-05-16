6ca6333deb86a3fcd2719813fd0f8e63794540ad
AgaevDanik <agaevdaniik@gmail.com>
2020-05-17 01:22:58 +0300

    tags, tag_detail, list_of connected posts with tag

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -6,10 +6,22 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
-        return reverse('post_detail_url', kwargs={'slug': self.slug}),
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -10,3 +10,13 @@ def posts_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
